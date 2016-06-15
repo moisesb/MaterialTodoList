c6270d4bc6c401ecffdd2c8d03a6addd83b14422
moises <moises.borges@gmail.com>
2016-06-14 22:00:48 -0300

    - Create Account and Login layout improved

--- app/src/main/java/com/borges/moises/materialtodolist/createaccount/CreateAccountActivity.java
+++ app/src/main/java/com/borges/moises/materialtodolist/createaccount/CreateAccountActivity.java
@@ -5,22 +5,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.FrameLayout;
 
 import com.borges.moises.materialtodolist.R;
 
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CreateAccountActivity extends AppCompatActivity{
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         ButterKnife.bind(this);
-
+        setToolbar();
         addFragment();
+    }
+
+    private void setToolbar() {
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     private void addFragment() {
--- app/src/main/java/com/borges/moises/materialtodolist/login/LoginActivity.java
+++ app/src/main/java/com/borges/moises/materialtodolist/login/LoginActivity.java
@@ -5,19 +5,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.borges.moises.materialtodolist.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity{
 
 
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        ButterKnife.bind(this);
+        setToolbar();
         addFragment();
+    }
+
+    private void setToolbar() {
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     private void addFragment() {
